cf6f519f1478fa08f4d9ef7f146fad659f457705
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2022-12-16 11:44:32 +0100

    fin

--- comparar_problemas.py
+++ comparar_problemas.py
@@ -4,9 +4,9 @@ import os
 import random
 import re
 import sys
-#
+
 # Complete the 'compareTriplets' function below.
-#
+
 # The function is expected to return an INTEGER_ARRAY.
 # The function accepts following parameters:
 # 1. INTEGER_ARRAY a
