54560d71b677abf33e7589985b572879ecf1abf5
Lidia Velicia <lvelirui@myuax.com>
2022-12-16 13:39:15 +0100

    reestructurar repo y añadir data

---
+++ EJ1_COMPARAR_PROBLEMAS/main01.py
@@ -0,0 +1,4 @@
+from lanzador01 import juego_comparacion
+
+if __name__ == '__main__':
+    juego_comparacion()
---
+++ EJ2_MANZANAS_NARANJAS/main02.py
@@ -0,0 +1,4 @@
+from lanzador02 import juego_manzanas_naranjas
+
+if __name__ == '__main__':
+    juego_manzanas_naranjas()
