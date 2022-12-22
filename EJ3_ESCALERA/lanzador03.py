66be417ba0715fabfd781b8a4795f3ff049ae1d0
luciacantos <luciacantos22@gmail.com>
2022-12-22 18:08:29 +0100

    definir variable dibujo de la escalera

--- EJ3_ESCALERA/lanzador03.py
+++ EJ3_ESCALERA/lanzador03.py
@@ -6,3 +6,10 @@ def escalera(n):
     '''
     for i in range(1, n+1):
         return"\n".join([" "*(n-i) + " #"*i for i in range(1, n+1)])
+
+def dibujo_escalera():
+    '''
+    Función que pide el número de pisos para la escalera
+    '''
+    altura = int(input("Introduce el número de pisos de la escalera: "))
+    return escalera(altura)
