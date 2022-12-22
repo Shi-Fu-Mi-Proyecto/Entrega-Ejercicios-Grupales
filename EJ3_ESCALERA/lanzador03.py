5d60beb827540e40c917dbd180244431486fcd94
luciacantos <luciacantos22@gmail.com>
2022-12-22 18:04:49 +0100

    definir función escalera

--- EJ3_ESCALERA/lanzador03.py
+++ EJ3_ESCALERA/lanzador03.py
@@ -1,2 +1,8 @@
 # Módulo con las funciones del ejercicio 3: 'Escalera'
 
+def escalera(n):
+    '''
+    Función que dibuja la escalera de n pisos
+    '''
+    for i in range(1, n+1):
+        return"\n".join([" "*(n-i) + " #"*i for i in range(1, n+1)])
